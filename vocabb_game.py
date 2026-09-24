2b4ebf78284cb11326512475f9d908d222c42be3
411-09-sununthicha <54250@yupparaj.ac.th>
2026-09-24 18:26:20 +0700

    Update vocabb_game.py

--- vocabb_game.py
+++ vocabb_game.py
@@ -1,7 +1,7 @@
 import time
 import streamlit as st
 
-st.title("⏱️ เกมเติมศัพท์จับเวลา")
+st.title("⏱️ เกมทายศัพท์หมวดของใช้จับเวลา")
 
 # 1. กำหนดค่าเริ่มต้นใน session_state ถ้ายังไม่มี
 if "ans1_val" not in st.session_state:
